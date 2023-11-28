21779bfaa4e4c1781569a77c50795dc2423c89eb
Ihor Palyvoda <ihor.palyvoda@perfsol.tech>
2023-11-28 10:01:07 +0200

    Initial Commit

---
+++ SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Igor Palyvoda on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
